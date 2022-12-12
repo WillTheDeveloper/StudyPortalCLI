8108c450743daf1e1c277e848522904ca9594229
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-12-12 17:34:36 +0000

    Created comment and post model

---
+++ Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StudyPortalCLI.Models;
+
+public class Comment
+{
+    [JsonPropertyName("comment")]
+    public string Commentt { get; set; } = null!;
+    [JsonPropertyName("user")]
+    public string User { get; set; } = null!;
+    [JsonPropertyName("post")]
+    public Post Post { get; set; } = null!;
+    [JsonPropertyName("commented")]
+    public DateTime Commented { get; set; }
+}
---
+++ Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StudyPortalCLI.Models;
+
+public class Post
+{
+    [JsonPropertyName("title")]
+    public string Title { get; set; } = null!;
+    [JsonPropertyName("body")]
+    public string Body { get; set; } = null!;
+    [JsonPropertyName("user")]
+    public string User { get; set; } = null!;
+    [JsonPropertyName("subject")]
+    public string Subject { get; set; } = null!;
+    [JsonPropertyName("views")]
+    public int Views { get; set; }
+    [JsonPropertyName("tag")]
+    public string? Tag { get; set; }
+}
