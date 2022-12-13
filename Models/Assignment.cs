5c0166ccba62e4fe7d817d6349b4cf098aca8d39
Will Burton <willburton@linney.com>
2022-12-13 13:55:44 +0000

    Constructed models for assignment and blog

--- Models/Assignment.cs
+++ Models/Assignment.cs
@@ -2,5 +2,10 @@
 
 public class Assignment
 {
-    
+    public string Title { get; set; }
+    public string Details { get; set; }
+    public DateTime Due { get; set; }
+    public DateTime Set { get; set; }
+    public Subject Subject { get; set; }
+    public DateTime Created { get; set; }
 }
--- Models/Blog.cs
+++ Models/Blog.cs
@@ -2,5 +2,11 @@
 
 public class Blog
 {
-    
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public string Type { get; set; }
+    public DateTime Posted { get; set; }
+    public User CreatedBy { get; set; }
+    public string Slug { get; set; }
+    public Uri Image { get; set; }
 }
