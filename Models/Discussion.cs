41ce13cc229783b306eb2fff982bd1e0581ea574
Will Burton <willburton@linney.com>
2022-12-13 13:59:53 +0000

    Completed discussion and group models

--- Models/Discussion.cs
+++ Models/Discussion.cs
@@ -2,5 +2,10 @@
 
 public class Discussion
 {
-    
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public Models.Group Group { get; set; }
+    public bool Locked { get; set; }
+    public User User { get; set; }
+    public DateTime Created { get; set; }
 }
--- Models/Group.cs
+++ Models/Group.cs
@@ -2,5 +2,7 @@
 
 public class Group
 {
-    
+    public string Name { get; set; }
+    public Subject Subject { get; set; }
+    public DateTime Created { get; set; }
 }
