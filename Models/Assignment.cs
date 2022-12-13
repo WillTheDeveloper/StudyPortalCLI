44bf96eee0f67d64e828a2cbaf02b37d65778786
Will Burton <willburton@linney.com>
2022-12-13 14:05:00 +0000

    Added json property names to assignment

--- Models/Assignment.cs
+++ Models/Assignment.cs
@@ -1,11 +1,19 @@
-﻿namespace StudyPortalCLI.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace StudyPortalCLI.Models;
 
 public class Assignment
 {
+    [JsonPropertyName("title")]
     public string Title { get; set; }
+    [JsonPropertyName("details")]
     public string Details { get; set; }
+    [JsonPropertyName("due")]
     public DateTime Due { get; set; }
+    [JsonPropertyName("set")]
     public DateTime Set { get; set; }
+    [JsonPropertyName("subject")]
     public Subject Subject { get; set; }
+    [JsonPropertyName("created")]
     public DateTime Created { get; set; }
 }
