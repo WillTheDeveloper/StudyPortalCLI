2429a1fd14eda6136962ea86f879de180db2c169
Will Burton <willburton@linney.com>
2022-12-14 11:39:46 +0000

    Made strings not nullable on assignment model

--- Models/Assignment.cs
+++ Models/Assignment.cs
@@ -4,16 +4,10 @@ namespace StudyPortalCLI.Models;
 
 public class Assignment
 {
-    [JsonPropertyName("title")]
-    public string Title { get; set; }
-    [JsonPropertyName("details")]
-    public string Details { get; set; }
-    [JsonPropertyName("due")]
-    public DateTime Due { get; set; }
-    [JsonPropertyName("set")]
-    public DateTime Set { get; set; }
-    [JsonPropertyName("subject")]
-    public Subject Subject { get; set; }
-    [JsonPropertyName("created")]
-    public DateTime Created { get; set; }
+    [JsonPropertyName("title")] public string Title { get; set; } = null!;
+    [JsonPropertyName("details")] public string Details { get; set; } = null!;
+    [JsonPropertyName("due")] public DateTime Due { get; set; }
+    [JsonPropertyName("set")] public DateTime Set { get; set; }
+    [JsonPropertyName("subject")] public Subject Subject { get; set; } = null!;
+    [JsonPropertyName("created")] public DateTime Created { get; set; }
 }
