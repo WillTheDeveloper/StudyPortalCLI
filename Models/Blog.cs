5eeaf4e0aa9bde94ad0210cb17c0bc3dcac77e7c
Will Burton <willburton@linney.com>
2022-12-14 12:19:38 +0000

    Added json names for blog model

--- Models/Blog.cs
+++ Models/Blog.cs
@@ -1,12 +1,14 @@
-﻿namespace StudyPortalCLI.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace StudyPortalCLI.Models;
 
 public class Blog
 {
-    public string Title { get; set; }
-    public string Body { get; set; }
-    public string Type { get; set; }
-    public DateTime Posted { get; set; }
-    public User CreatedBy { get; set; }
-    public string Slug { get; set; }
-    public Uri Image { get; set; }
+    [JsonPropertyName("title")] public string Title { get; set; } = null!;
+    [JsonPropertyName("body")] public string Body { get; set; } = null!;
+    [JsonPropertyName("type")] public string Type { get; set; } = null!;
+    [JsonPropertyName("posted")] public DateTime Posted { get; set; }
+    [JsonPropertyName("created")] public User CreatedBy { get; set; } = null!;
+    [JsonPropertyName("slug")] public string Slug { get; set; } = null!;
+    [JsonPropertyName("image")] public Uri Image { get; set; } = null!;
 }
