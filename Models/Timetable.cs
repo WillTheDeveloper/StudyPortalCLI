8857df8d4c2e4fbbc26b01525febcf18c4b70dba
Will Burton <willburton@linney.com>
2022-12-28 12:14:58 +0000

    Added initial models for timetable and webhook

--- Models/Timetable.cs
+++ Models/Timetable.cs
@@ -2,5 +2,9 @@
 
 public class Timetable
 {
-    
+    public Subject Subject { get; set; }
+    public DateTime Start { get; set; }
+    public DateTime End { get; set; }
+    public string Day { get; set; }
+    public string Room { get; set; }
 }
--- Models/Webhook.cs
+++ Models/Webhook.cs
@@ -2,5 +2,17 @@
 
 public class Webhook
 {
-    
+    public string Name { get; set; }
+    public Uri Url { get; set; }
+    public User User { get; set; }
+    public Boolean Active { get; set; }
+    public Triggers Triggers { get; set; }
+}
+
+public class Triggers
+{
+    public Post Posts { get; set; }
+    public Comment Comments { get; set; }
+    public Assignment Assignments { get; set; }
+    public Blog Blogs { get; set; }
 }
