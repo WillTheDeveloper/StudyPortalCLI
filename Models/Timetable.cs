eabc84143a89876f14b1b34d3beb0d3e704ab1d7
Will Burton <willburton@linney.com>
2022-12-29 09:11:13 +0000

    Added nullables to timetable model

--- Models/Timetable.cs
+++ Models/Timetable.cs
@@ -2,9 +2,9 @@
 
 public class Timetable
 {
-    public Subject Subject { get; set; }
+    public Subject Subject { get; set; } = null!;
     public DateTime Start { get; set; }
     public DateTime End { get; set; }
-    public string Day { get; set; }
-    public string Room { get; set; }
+    public string Day { get; set; } = null!;
+    public string Room { get; set; } = null!;
 }
